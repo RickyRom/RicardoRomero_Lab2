839b9bbccbf2c8edbd466b2acf000c78fdd168e8
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-10-20 14:36:50 -0600

    avance de la clase mensajes

--- RicardoRomero_Lab2/src/ricardoromero_lab2/Mensaje.java
+++ RicardoRomero_Lab2/src/ricardoromero_lab2/Mensaje.java
@@ -10,5 +10,66 @@ package ricardoromero_lab2;
  * @author ricky
  */
 public class Mensaje {
+    private String titulo;
+    private String emisor;
+    private String receptor;
+    private String cuerpo;
+    private int nivel_importancia;
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String titulo, String emisor, String receptor, String cuerpo, int nivel_importancia) {
+        this.titulo = titulo;
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.cuerpo = cuerpo;
+        this.nivel_importancia = nivel_importancia;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(String receptor) {
+        this.receptor = receptor;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public int getNivel_importancia() {
+        return nivel_importancia;
+    }
+
+    public void setNivel_importancia(int nivel_importancia) {
+        this.nivel_importancia = nivel_importancia;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
     
 }
